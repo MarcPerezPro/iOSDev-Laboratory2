a28317a6c3dff389ac7ebf2afaaa3d92b3ab0b34
Marc PEREZ <marc.perez@epitech.eu>
2020-04-05 14:00:52 +0200

    Added a new tab

---
+++ lab2/RespondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RespondViewController.swift
+//  lab2
+//
+//  Created by Marc PEREZ on 5/4/2020.
+//  Copyright © 2020 Marc PEREZ. All rights reserved.
+//
+
+import UIKit
+
+class RespondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
