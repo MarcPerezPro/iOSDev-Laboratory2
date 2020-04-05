98c884a76d4805fc612ded0761fd8260cf0539db
Marc PEREZ <marc.perez@epitech.eu>
2020-04-05 13:17:05 +0200

    Initial Commit

---
+++ lab2/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  lab2
+//
+//  Created by Marc PEREZ on 5/4/2020.
+//  Copyright © 2020 Marc PEREZ. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
