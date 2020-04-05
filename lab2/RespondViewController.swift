e62cc1269f0750f5e7c815e37e2c53912b539aa8
Marc PEREZ <marc.perez@epitech.eu>
2020-04-05 23:12:41 +0200

    Adding the IBOutlets + the filter

--- lab2/RespondViewController.swift
+++ lab2/RespondViewController.swift
@@ -10,10 +10,29 @@ import UIKit
 
 class RespondViewController: UIViewController {
 
+    @IBOutlet weak var textField: LimitedTextField!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var copyTextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.textField.delegate = self
     }
+}
 
+extension RespondViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
 
+        // Verify all the conditions
+        if let limitedTextField = textField as? LimitedTextField {
+            return limitedTextField.verifyFields(shouldChangeCharactersIn: range, replacementString: string)
+        }
+        return true
+    }
 }
