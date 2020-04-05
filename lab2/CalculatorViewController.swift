d568398e6b37f17da11097fd1a3e9d9d7176fbdf
Marc PEREZ <marc.perez@epitech.eu>
2020-04-06 01:13:11 +0200

    Calculator limited to ints

--- lab2/CalculatorViewController.swift
+++ lab2/CalculatorViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
 
+    @IBOutlet weak var value1: LimitedTextField!
+    @IBOutlet weak var value2: LimitedTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.value1.delegate = self
+        self.value1.valueType = .onlyNumbers
+        self.value1.maxLength = 4
+        self.value2.delegate = self
+        self.value2.valueType = .onlyNumbers
+        self.value2.maxLength = 4
     }
 
 
 }
 
+extension CalculatorViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+
+        // Verify all the conditions
+        if let limitedTextField = textField as? LimitedTextField {
+            let value = limitedTextField.verifyFields(shouldChangeCharactersIn: range, replacementString: string)
+            if value == false {
+                // Play the shake animation
+                limitedTextField.shake()
+            }
+            return value
+        }
+        return true
+    }
+}
